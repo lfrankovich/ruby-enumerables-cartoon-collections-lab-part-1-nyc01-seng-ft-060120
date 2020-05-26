fb951eda65fd1558531738ba9a9798ba8758d1b1
Lauren Frankovich <frankovister@gmail.com>
2020-05-26 21:16:06 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,7 +12,7 @@ def list_dwarves(array)
   #
   # Print a numbered list of each element
   array.each do |element|
-    puts "#{element.count}. element"
+    puts "#{element.count}. #{element}"
   end
 end
 
