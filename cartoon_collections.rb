0f6911ac432f10d22ebcab15bc94b8e262c23da3
Lauren Frankovich <frankovister@gmail.com>
2020-05-26 21:08:24 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -3,14 +3,8 @@ def greet_characters(array)
   #
   # Print a custom greeting for each element
   array.each do |element|
-    puts
+    puts "Hello ${element}"
 end
-
-
-	oppressed_workers = ["dopey", "sneezy", "happy", "angry", "doc", "lemonjello", "sleepy" ]
-	oppressed_workers.each do |oppressed_worker|
-	puts "#{oppressed_worker.capitalize} wants to start a union!"
-end #=>
 
 def list_dwarves(array)
   # Use `each_with_index` to enumerate over the provided array
